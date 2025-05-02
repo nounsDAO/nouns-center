c7eadf3f86f63f2b18b6621c7f1e1c76d2b17f7e
Volky <volkyeth@pm.me>
2025-05-02 17:14:04 -0300

    chore: comment out tailwind plugins to run @tailwindcss/upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,5 +58,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/forms')],
+  // plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/forms')],
 };
