eaf6ed743f0311c6ff5c1988feeb3f39de5c8116
Volky <volkyeth@pm.me>
2025-05-02 14:11:10 -0300

    refactor: remove unused dotenv

--- scripts/build-search.js
+++
@@ -1,9 +0,0 @@
-// build-search.js
-const dotenv = require('dotenv');
-
-(async function () {
-  // initialize environment variables
-  dotenv.config();
-
-  // console.log("Schnitzel! Let's fetch some data!");
-})();
