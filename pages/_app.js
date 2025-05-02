c354eaa5d5d646a381aae19b60341d9a4bc42295
Volky <volkyeth@pm.me>
2025-05-02 14:07:11 -0300

    fix: remove leftover wagmi and rainbowkit providers

--- pages/_app.js
+++ pages/_app.js
@@ -2,33 +2,10 @@ import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { publicProvider } from 'wagmi/providers/public';
-
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
-
-const { chains, provider } = configureChains([chain.mainnet], [publicProvider()]);
-
-const { connectors } = getDefaultWallets({
-  appName: 'Nouns Center',
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
-
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains} coolMode>
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </WagmiConfig>
-      {/* {asPath === "/" && <div id="autocomplete"></div>} */}
+      <Component {...pageProps} />
     </Layout>
   );
 }
